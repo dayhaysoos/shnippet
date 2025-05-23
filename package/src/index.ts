0b7c9c067c7d07e07188addbd5aeb3dfe7a020f4
Nick DeJesus <ndejesus1227@gmail.com>
2025-05-23 14:50:26 -0400

    remove unused stuff

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,3 +1,2 @@
 import SnippetExtractor from "./extract/SnippetExtractor.js";
-import { getSnippet } from "./helpers/getSnippet.js";
-export { SnippetExtractor, getSnippet };
+export { SnippetExtractor };
