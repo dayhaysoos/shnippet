acfb10c3b56409785045904af8fca1f57b38cb30
GitHub Action <action@github.com>
2025-05-30 22:30:18 +0000

    style: format code

--- package/test/__fixtures__/example.js
+++ package/test/__fixtures__/example.js
@@ -1,17 +1,17 @@
-"use strict";
+'use strict';
 // Example TypeScript file with snippets
 //:snippet-start: example1
 function sayHello() {
-    console.log('Hello from TypeScript!');
+  console.log('Hello from TypeScript!');
 }
 //:snippet-end:
 //:snippet-start: example2
 class Example {
-    constructor() {
-        this.message = 'This is a TypeScript class';
-    }
-    getMessage() {
-        return this.message;
-    }
+  constructor() {
+    this.message = 'This is a TypeScript class';
+  }
+  getMessage() {
+    return this.message;
+  }
 }
 //:snippet-end:
